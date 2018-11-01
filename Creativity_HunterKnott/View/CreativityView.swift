7f7cc1816f3be5f6214e350d781da7b204aec8fc
HunterKnott <hunterknott00@gmail.com>
2018-11-01 12:30:07 -0600

    Created CreativityView file (cocoa) in view folder

---
+++ Creativity_HunterKnott/View/CreativityView.swift
@@ -0,0 +1,23 @@
+//
+//  CreativityView.swift
+//  Creativity_HunterKnott
+//
+//  Created by Knott, Hunter on 11/1/18.
+//  Copyright © 2018 Knott, Hunter. All rights reserved.
+//
+
+import UIKit
+
+public class CreativityView: UIView
+{
+
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    public override func draw(_ rect: CGRect) -> Void
+    {
+        // Drawing code
+    }
+    
+
+}
