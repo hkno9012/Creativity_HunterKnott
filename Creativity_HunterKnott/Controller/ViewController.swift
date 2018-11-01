9650a91107abb8cf3463dfbba1861b5f56c40873
HunterKnott <hunterknott00@gmail.com>
2018-11-01 11:50:33 -0600

    Initial Commit

---
+++ Creativity_HunterKnott/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Creativity_HunterKnott
+//
+//  Created by Knott, Hunter on 11/1/18.
+//  Copyright © 2018 Knott, Hunter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
