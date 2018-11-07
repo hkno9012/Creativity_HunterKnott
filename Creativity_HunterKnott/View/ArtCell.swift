4ce147dc0c17b02228403b0988b3281a18020736
HunterKnott <hunterknott00@gmail.com>
2018-11-07 13:35:06 -0700

    Connected image and label to code

---
+++ Creativity_HunterKnott/View/ArtCell.swift
@@ -0,0 +1,15 @@
+//
+//  ArtCell.swift
+//  Creativity_HunterKnott
+//
+//  Created by Knott, Hunter on 11/7/18.
+//  Copyright © 2018 Knott, Hunter. All rights reserved.
+//
+
+import UIKit
+
+class ArtCell: UICollectionViewCell
+{
+    @IBOutlet weak var artImage: UIImageView!
+    @IBOutlet weak var artLabel: UILabel!
+}
